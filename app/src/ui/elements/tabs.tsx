c3c1ea0241026f1bffa41bc6726630bf6d250e24
redrocket94 <oliver.duedam@gmail.com>
2024-11-05 05:04:25 +0000

    fix: adjust height of TabsList component for improved layout on mobile

--- app/src/ui/elements/tabs.tsx
+++ app/src/ui/elements/tabs.tsx
@@ -12,7 +12,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-12 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+      "inline-flex h-8 lg:h-12 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
       className,
     )}
     {...props}
